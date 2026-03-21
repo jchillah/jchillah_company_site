db505fa0700aabb48ab1271616898f5d3a53d768
Jchillah <jchillah@gmail.com>
2026-03-21 01:18:26 +0100

    🚀 Deploy on 2026-03-21T01:18:25.797361

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1190349144" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3658293232" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
